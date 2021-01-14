44d7d5c70eefb234cb32ad67da370f93ba79acd0
github username <rdg.brg@gmail.com>
2021-01-14 15:44:04 -0500

    Set up users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,25 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+  wrap_parameters :user, include: [:first_name, :last_name, :email, :password]
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create 
+    # byebug
+    user = User.create(user_params)
+
+    if user.valid?
+      render json: { user: UserSerializer.new(user) }, status: :created
+    else
+      render json: { message: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
