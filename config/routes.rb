85fa8e38e8b0db5c68e1198db9ad36299b99e878
github username <rdg.brg@gmail.com>
2021-01-14 15:44:26 -0500

    Create users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      
       resources :orders
+      
       resources :products
-      resources :users
+      
+      resources :users, only: [:index]
+      post '/signup', to: 'users#create'
     end
   end
 end
