4fe6142f731e2f6752d052ea170705dc9bc2dd50
github username <rdg.brg@gmail.com>
2021-01-14 19:31:19 -0500

    Fix sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       
+      post '/login', to: 'sessions#login'
+      get '/autologin', to: 'sessions#autologin'
+
       resources :orders
       
       resources :products
