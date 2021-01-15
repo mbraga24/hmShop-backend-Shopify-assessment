373fe76ad0087040821de02ab9b66c83f013aa96
github username <rdg.brg@gmail.com>
2021-01-14 21:38:57 -0500

    Add JWT encoding and decoding for security

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,18 +2,32 @@ class Api::V1::SessionsController < ApplicationController
   wrap_parameters :user, include: [:email, :password] 
 
   def login
-    # byebug
     user = User.find_by(email: user_credentials_params[:email])
 
     if user && user.authenticate(user_credentials_params[:password])
-      render json: { user: UserSerializer.new(user), success: "Welcome back #{user.first_name} #{user.last_name}!" }, status: :accepted
+      token = JWT.encode({ user_id: user.id }, 'a_big_secret', 'HS256')
+      render json: { user: UserSerializer.new(user), token: token, success: "Welcome back, #{user.first_name}!" }, status: :accepted
     else
       render json: { error: user.error.full_messages, header: "Invalid email or password" }, status: :unauthorized
     end
   end
 
   def autologin
-    byebug
+    auth_header = request.headers['Authorization']
+
+    token = auth_header.split(" ")[1]
+
+    decoded_token = JWT.decode(token, 'a_big_secret', true, { algorithm: 'HS256' })
+
+    user_id = decoded_token[0]["user_id"]
+
+    user = User.find_by(id: user_id)
+
+    if user 
+      render json: { user: UserSerializer.new(user) }, status: :ok
+    else
+      render json: { header: "You are not logged in." }
+    end
   end
 
   private 
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,9 @@ class Api::V1::UsersController < ApplicationController
     user = User.create(user_params)
 
     if user.valid?
-      render json: { user: UserSerializer.new(user), success: "Welcome to HmShop #{user.first_name} #{user.last_name}!" }, status: :created
+
+      token = JWT.encode({ user_id: user.id }, 'a_big_secret', 'HS256')
+      render json: { user: UserSerializer.new(user), token: token, success: "Hooray! Welcome to HmShop, #{user.first_name}!" }, status: :created
     else
       render json: { header: "Check the error(s) bellow:" , error: user.errors.full_messages,  }, status: :bad_request
     end
