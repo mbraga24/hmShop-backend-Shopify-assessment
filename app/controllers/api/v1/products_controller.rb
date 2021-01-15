9948d9b32f87e2def2aad10c62893b7b0c23f48c
github username <rdg.brg@gmail.com>
2021-01-15 16:44:53 -0500

    Add index and create actions to products_controllers

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :authenticate, only: [:create, :index]
+
+  def index 
+    products = Product.all
+
+    render json: products
+  end
+
+
+  def create
+
+  end
+
+
 end
