1fca74592d458277f82cf70259aba3496a934b7f
github username <rdg.brg@gmail.com>
2021-01-17 14:55:01 -0500

    Structure  serializer to deliver proper data - product, seller and buye r

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,23 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :product, :seller, :buyer
   has_one :user
   has_one :product
+
+  def seller
+    seller = User.find_by(id: object.product.user_id)
+    return UserSerializer.new(seller)
+  end
+
+  def buyer
+    buyer = object.user
+    return UserSerializer.new(buyer)
+  end
+
+  def product 
+    return {
+      name: object.product.name,
+      price: object.product.price,
+      image_url: object.product.image_url
+    }
+  end
 end
