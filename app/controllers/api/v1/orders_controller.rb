8c08c6e45d2d90765dc4cfc525f6d80660312195
github username <rdg.brg@gmail.com>
2021-01-17 14:58:13 -0500

    Sending proper data to frontend

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::OrdersController < ApplicationController
-  skip_before_action :authenticate, only: []
+  skip_before_action :authenticate, only: [:index]
+
+  def index
+    orders = Order.all
+    render json: orders
+  end
 
   def user_orders
-    
     orders = Order.where(user: @current_user)
-
-    # ============================================================
-    #            NEEDS TO SERIALIZE THE ARRAY OF RECORDS
-    # ============================================================
-
-    render json: each_serializer: orders
+    render json: {
+      orders: orders.map { |order|
+        OrderSerializer.new(order).attributes
+      }
+    }
   end
 end
